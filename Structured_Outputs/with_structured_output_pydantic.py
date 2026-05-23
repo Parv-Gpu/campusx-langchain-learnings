17cfc847e1164929bd2710c9ac35938929d994e5
Parv <parvgupta854@gmail.com>
2026-05-23 14:50:50 +0530

    updated project

--- Structured_Outputs/with_structured_output_pydantic.py
+++ Structured_Outputs/with_structured_output_pydantic.py
@@ -1,20 +1,21 @@
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 from typing import TypedDict, Annotated, Optional, Literal
+from pydantic import BaseModel, Field
 
 load_dotenv()
 
-model = ChatGroq(model="llama-3.3-70b-versatile")
+model = ChatOpenAI()
 
 # schema
-class Review(TypedDict):
+class Review(BaseModel):
 
-    key_themes: Annotated[list[str], "Write down all the key themes discussed in the review in a list"]
-    summary: Annotated[str, "A brief summary of the review"]
-    sentiment: Annotated[Literal["pos", "neg"], "Return sentiment of the review either negative, positive or neutral"]
-    pros: Annotated[Optional[list[str]], "Write down all the pros inside a list"]
-    cons: Annotated[Optional[list[str]], "Write down all the cons inside a list"]
-    name: Annotated[Optional[str], "Write the name of the reviewer"]
+    key_themes: list[str] = Field(description="Write down all the key themes discussed in the review in a list")
+    summary: str = Field(description="A brief summary of the review")
+    sentiment: Literal["pos", "neg"] = Field(description="Return sentiment of the review either negative, positive or neutral")
+    pros: Optional[list[str]] = Field(default=None, description="Write down all the pros inside a list")
+    cons: Optional[list[str]] = Field(default=None, description="Write down all the cons inside a list")
+    name: Optional[str] = Field(default=None, description="Write the name of the reviewer")
     
 
 structured_model = model.with_structured_output(Review)
@@ -34,6 +35,4 @@ S-Pen support is unique and useful
 Review by Nitish Singh
 """)
 
-print(result['name'])
-print(result['summary'])
-print(result['sentiment'])
+print(result)
